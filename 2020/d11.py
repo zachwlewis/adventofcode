01c32e0d687d8789e20dcbd84f4f9d9ee3e0bf48
Zachary Lewis <zachwlewis@gmail.com>
2020-12-11 10:50:31 -0800

    d11

---
+++ 2020/d11.py
@@ -0,0 +1,14 @@
+## adventofcode.com
+# Day 11
+# https://adventofcode.com/2020/day/11
+
+import fr
+from typing import List, Dict
+
+answer1 = 'unknown'
+answer2 = 'unknown'
+
+inputs = fr.readAsList('input11-test')
+
+print(f'Answer 1: {answer1}')
+print(f'Answer 2: {answer2}')
