ad9ada75baa9b97006ebf978a51568aabb42f3c4
Zachary Lewis <zachwlewis@gmail.com>
2020-12-14 23:40:33 -0800

    Day 6, Part 2

--- 2020/d14.py
+++ 2020/d14.py
@@ -51,6 +51,54 @@ answer1 = 0
 for value in mem.values():
   answer1 += value
 
+# PART 2 ---------------------
+def mask36_2(bstr: str, _mask: str) -> str:
+  """Masks a 36-character binary string with the provided mask."""
+  if len(bstr) != len(_mask):
+    print("Error: Binary/Mask Length Mismatch")
+    return "err"
+
+  blist = list(bstr)
+  for i in range(len(_mask)):
+    blist[i] = _mask[i] if _mask[i] != '0' else bstr[i]
+
+  return ''.join(blist)
+def buildAddresses(addr: str) -> list[int]:
+  if not 'X' in addr: return [b36toi(addr)]
+
+  i = addr.index('X')
+  one = f'{addr[0:i]}1{addr[i+1:]}'
+  ones = buildAddresses(one)
+  zero = f'{addr[0:i]}0{addr[i+1:]}'
+  zeros = buildAddresses(zero)
+
+  return ones + zeros
+
+mem = {}
+
+for line in inputs:
+  _line = line.split(' ')
+  if _line[0] == 'mask':
+    mask = _line[2]
+    continue
+
+  # Non-mask lines in the format:
+  # |     0     |1|   2   |
+  #  mem[<addr>] = <value>
+
+  addr = int(_line[0][4:-1])
+  value = int(_line[2])
+
+  baddr = itob36(addr)
+  baddr = mask36_2(baddr, mask)
+
+  addrs = buildAddresses(baddr)
+
+  for a in addrs: mem[a] = value
+
+answer2 = 0
+for value in mem.values():
+  answer2 += value
 
 print(f'Answer 1: {answer1}')
 print(f'Answer 2: {answer2}')
