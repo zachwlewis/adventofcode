9bdf579d6c98d25c1373415d79211aa82c6bd64e
Zachary Lewis <zachwlewis@gmail.com>
2022-12-07 15:55:33 -0800

    Update br.zsh

--- 2022/br.zsh
+++ 2022/br.zsh
@@ -1,5 +1,6 @@
 #!/bin/zsh
 
+set -e
 echo "Starting build of $( pwd; )/$1.cpp"
 /usr/bin/clang++ -std=c++2a -stdlib=libc++ -fcolor-diagnostics -fansi-escape-codes -g $( pwd; )/$1.cpp -o $( pwd; )/out/$1
 echo
