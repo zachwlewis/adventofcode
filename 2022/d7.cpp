a3f7d5d29b0823677cc252110a9113d9d859e569
Zachary Lewis <zachwlewis@gmail.com>
2022-12-07 12:23:08 -0800

    d7: can now parse file

--- 2022/d7.cpp
+++ 2022/d7.cpp
@@ -17,104 +17,118 @@
 class File
 {
 public:
-  File(std::string inName, char inMode, size_t inSize)
-  : mName(inName), mMode(inMode), mSize(inSize) {}
+	File() : mName(""), mMode('d'), mSize(0) {}
+	File(std::string inName, char inMode, size_t inSize)
+		: mName(inName), mMode(inMode), mSize(inSize) {}
+	const static char DIR_FLAG = 'd';
+	const static char FILE_FLAG = '-';
+	static File mkdir(std::string inName) { return File(inName, File::DIR_FLAG, 0); }
+	static File touch(std::string inName, size_t inSize) { return File(inName, File::FILE_FLAG, inSize); }
 
-  //static File mkdir(std::string inName) { return File(inName, 'd', 0); }
-  //static File touch(std::string inName, size_t inSize) { return File(inName, '-', inSize); }
+	std::vector<File*> children;
+	std::string_view name() const { return mName; }
+	size_t size() const
+	{
+		if (is_file())
+			return mSize;
 
-  
-  std::map<std::string, File> children;
-  File* mkdir(std::string inName)
-  {
-    children[inName] = File(inName, 'd', 0);
-    return &children[inName];
-  }
-  File* touch(std::string inName, size_t inSize)
-  {
-    children[inName] = File(inName, '-', inSize);
-    return &children[inName];
-  }
-  std::string_view name() const { return mName; }
-  size_t size() const
-  {
-    if (!dir())
-      return mSize;
-
-    size_t dir_size = 0;
-    for (auto file : children)
-    {
-      dir_size += file.second.size();
-    }
-    return dir_size;
-  }
-  bool dir() const { return mMode == 'd'; }
+		size_t dir_size = 0;
+		for (auto file : children)
+		{
+			dir_size += file->size();
+		}
+		return dir_size;
+	}
+	bool is_dir() const { return mMode == File::DIR_FLAG; }
+	bool is_file() const { return mMode == File::FILE_FLAG; }
 private:
-  std::string mName = "";
-  size_t mSize = 0;
-  char mMode = '-';
+	std::string mName = "";
+	size_t mSize = 0;
+	char mMode = '-';
 
 };
 
-std::string pwd(const std::vector<File*> &inPath)
+void pwd(const std::vector<File*>& inPath)
 {
-  std::stringstream path;
+	for (auto file : inPath)
+	{
+		std::cout << "\033[34;7m" << file->name() << "\033[0m";
+		std::cout << "/";
+	}
 
-  for (std::vector<File*>::const_iterator iter = inPath.begin(); iter < inPath.end(); ++iter)
-  {
-    path << "\033[34;7m" << (*iter)->name() << "\033[0m";
-    path << "/";
-  }
-
-  return path.str();
+	std::cout << std::endl;
 }
 
-void print_disk(const std::vector<File> &inDisk)
+void print_disk(const std::vector<File>& inDisk)
 {
-  for (auto iter = inDisk.begin(); iter < inDisk.end(); ++iter)
-  {
-    std::cout << (*iter).name() << " ";
-    if ((*iter).dir()) 
-      std::cout << "dir" << std::endl;
-    else
-     std::cout << (*iter).size() << std::endl;
-  }
-  
+	for (auto iter = inDisk.begin(); iter < inDisk.end(); ++iter)
+	{
+		std::cout << (*iter).name() << " ";
+		if ((*iter).is_dir())
+			std::cout << "dir" << std::endl;
+		else
+			std::cout << (*iter).size() << std::endl;
+	}
 }
 
 int main() {
 
-  std::ifstream input_file (INPUT_FILE);
+	std::ifstream input_file(INPUT_FILE);
 
-  if (!input_file.is_open())
-  {
-    std::cout << "Unable to open input file: " << INPUT_FILE << std::endl;
-    return 1;
-  }
+	if (!input_file.is_open())
+	{
+		std::cout << "Unable to open input file: " << INPUT_FILE << std::endl;
+		return 1;
+	}
 
-  int score1 = 0;
-  int score2 = 0;
+	int score1 = 0;
+	int score2 = 0;
 
-  std::string input = "";
-  File root = File("/", 'd', 0);
-  std::vector<File*> path = {&root};
-  std::cout << pwd(path) << std::endl;
-  File* dir = root.mkdir("one");
-  root.touch("porn.jpg", 100);
-  root.touch("taxes.pdf", 50);
-  dir = root.mkdir("two");
-  dir->touch("coin.png", 300);
+	std::vector<File> disk = {};
+	std::vector<File*> path = {};
 
-  std::cout << root.size() << std::endl;
+	std::string input = "";
 
-  // while (input_file.good())
-  // {
-  //   std::getline(input_file, input);
-  //   std::cout << input << std::endl;
-  // }
+	while (input_file.good())
+	{
+		std::vector<std::string> tokens;
+		std::getline(input_file, input);
+		if (input.length() == 0)
+			break;
 
-  std::cout << "Part 1: " << score1 << std::endl;  
-  std::cout << "Part 2: " << score2 << std::endl;
+		std::string buf;
+		std::stringstream ss(input);
+		while (ss >> buf)
+		{
+			tokens.push_back(buf);
+		}
+		
+		if (tokens[0] == "$")
+		{
+			// new command for working directory
+			std::cout << "command: " << tokens[1] << std::endl;
+			continue;
+		}
+		
+		if (tokens[0] == "dir")
+		{
+			// add a directory
+			std::cout << "new directory: " << tokens[1] << std::endl;
+			continue;
+		}
 
-  return 0;
+		if (size_t file_size = std::stoi(tokens[0]))
+		{
+			// add a file
+			std::cout << "new file: " << tokens[1] << " (" << file_size << ")" << std::endl;
+			continue;
+		}
+
+		std::cout << "bad input" << std::endl;
+	}
+
+	std::cout << "Part 1: " << score1 << std::endl;
+	std::cout << "Part 2: " << score2 << std::endl;
+
+	return 0;
 }
